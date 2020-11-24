8b548e26c92532cfcdb659af4a1ad142a1092cce
alwaysluigi <35355408+alwaysluigi@users.noreply.github.com>
2020-11-24 12:05:31 -0600

    madlibs added

---
+++ madlibs.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+
+function libs() {
+    var firstAdj = document.getElementById("firstAdj").value;
+    document.querySelector('.adjOne').innerText = firstAdj;
+
+    var secAdj = document.getElementById("secAdj").value;
+    document.querySelector('.adjTwo').innerText = secAdj;
+    
+    var firstCol = document.getElementById("firstCol").value;
+    document.querySelector('.colorOne').innerText = firstCol;
+    
+    var firstVerb = document.getElementById("firstVerb").value;
+    document.querySelector('.verbOne').innerText = firstVerb;
+    
+    var thirdAdj = document.getElementById("thirdAdj").value;
+    document.querySelector('.adjThree').innerText = thirdAdj;
+    
+    var firstVeg = document.getElementById("firstVeg").value;
+    document.querySelector('.vegOne').innerText = firstVeg;
+    
+    var firstExclamation = document.getElementById("firstExclamation").value;
+    document.querySelector('.exclamationOne').innerText = firstExclamation;
+    
+    var firstVerbing = document.getElementById("firstVerbing").value;
+    document.querySelector('.verbingOne').innerText = firstVerbing;
+    
+    var secondVerbing = document.getElementById("secondVerbing").value;
+    document.querySelector('.verbingTwo').innerText = secondVerbing;
+    
+    var firstNoun = document.getElementById("firstNoun").value;
+    document.querySelector('.nounOne').innerText = firstNoun;
+    
+    var secVerb = document.getElementById("secVerb").value;
+    document.querySelector('.verbTwo').innerText = secVerb;
+    
+    var firstPlace = document.getElementById("firstPlace").value;
+    document.querySelector('.placeOne').innerText = firstPlace;
+    
+    var fourthAdj = document.getElementById("fourthAdj").value;
+    document.querySelector('.adjFour').innerText = fourthAdj;
+    
+    var firstNum = document.getElementById("firstNum").value;
+    document.querySelector('.numOne').innerText = firstNum;
+    
+    var thirdVerb = document.getElementById("thirdVerb").value;
+    document.querySelector('.verbThree').innerText = thirdVerb;
+    
+    var secNoun = document.getElementById("secNoun").value;
+    document.querySelector('.nounTwo').innerText = secNoun;
+    
+    var thirdNoun = document.getElementById("thirdNoun").value;
+    document.querySelector('.nounThree').innerText = thirdNoun;
+    
+    var fourthNoun = document.getElementById("fourthNoun").value;
+    document.querySelector('.nounFour').innerText = fourthNoun;
+    
+    var secVeg = document.getElementById("secVeg").value;
+    document.querySelector('.vegTwo').innerText = secVeg;
+    
+    var firstPerson = document.getElementById("firstPerson").value;
+    document.querySelector('.personOne').innerText = firstPerson;
+    
+    var secExclamation = document.getElementById("secExclamation").value;
+    document.querySelector('.exclamationTwo').innerText = secExclamation;
+    
+    var fifthAdj = document.getElementById("fifthAdj").value;
+    document.querySelector('.adjFive').innerText = fifthAdj;
+    
+    var fifthNoun = document.getElementById("fifthNoun").value;
+    document.querySelector('.nounFive').innerText = fifthNoun;
+
+}
+
